8ea507cb70787366dc3990e9ee3bc8fc26b2e667
Zachary Marazita <marazitazachary@gmail.com>
2019-03-05 09:58:04 -0500

    TicketDAO add() method complete

--- Project 1 STS Workspace/Zachary-Jaitee-Project1/src/main/java/com/revature/dao/TicketDAO.java
+++ Project 1 STS Workspace/Zachary-Jaitee-Project1/src/main/java/com/revature/dao/TicketDAO.java
@@ -2,6 +2,7 @@ package com.revature.dao;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -100,8 +101,50 @@ public class TicketDAO implements DAO<Ticket>{
 
 	@Override
 	public Ticket add(Ticket obj) {
-		// TODO Auto-generated method stub
-		return null;
+	
+		try(Connection conn = ConnectionFactory.getInstance().getConnection()){
+			conn.setAutoCommit(false);
+			
+			String [] keys = new String[1];
+			keys[0] = "reimb_id";
+			//(id, amount, time submitted, time resolved, descr, recipt, author, resolver, status id, type id)
+			PreparedStatement pstmt = conn.prepareStatement("INSERT INTO ers_reimbursement VALUES (0,?,null,null,?,null,?,null,0,?)", keys);
+			pstmt.setDouble(1, obj.getAmount());
+			pstmt.setString(2, obj.getTicketDescription());
+			pstmt.setInt(3, obj.getAuthorId());
+			switch(obj.getType()) {
+				case LODGING:
+					pstmt.setInt(4, 1);
+					break;
+				case TRAVEL:
+					pstmt.setInt(4, 2);
+					break;
+				case FOOD:
+					pstmt.setInt(4, 3);
+					break;
+				case OTHER:
+					pstmt.setInt(4, 4);
+					break;
+				default:
+					return null;
+			}
+			
+			int rowsInserted = pstmt.executeUpdate();
+			ResultSet rs = pstmt.getGeneratedKeys();
+			if(rowsInserted != 0) {
+				while(rs.next()) {
+					obj.setReimbId(rs.getInt(1));
+				}
+				conn.commit();
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		if(obj.getReimbId() == 0) {
+			return null;
+		}
+		return obj;
 	}
 
 	@Override
