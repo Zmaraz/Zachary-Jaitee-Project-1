e34e7546e3a26165e50abb0b9c380687d8ac3ff5
Zachary Marazita <marazitazachary@gmail.com>
2019-03-04 21:37:10 -0500

    cleaned up imports and driver

--- Project 1 STS Workspace/Zachary-Jaitee-Project1/src/main/java/com/revature/dao/UserDAO.java
+++ Project 1 STS Workspace/Zachary-Jaitee-Project1/src/main/java/com/revature/dao/UserDAO.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.List;
 
 import com.revature.models.User;
 import com.revature.models.enums.UserRole;
