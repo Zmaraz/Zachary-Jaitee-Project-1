6556c6daf0b44817240a5af58bef942d9ee6036f
Jaitee Pitts <jaiteepitts@gmail.com>
2019-03-04 20:58:52 -0500

    fixed get info from users

--- SQL/p1-population.sql
+++ SQL/p1-population.sql
@@ -25,26 +25,10 @@ INSERT INTO ers_reimbursement VALUES (
     0, 39.99, NULL, NULL,'hahaha',NULL,1,21,2,4);
 
 --get all info from users
-SELECT *
+SELECT ers_user_id, ers_username, ers_password, user_first_name, user_last_name, user_email, r.user_role
 FROM ers_users u
 JOIN ers_user_roles r
 ON u.user_role_id = r.ers_user_role_id;
-
---get all info about reimbursement
---maybe make a procedure
-
-SELECT reimb_id, reimb_amount, reimb_submitted, reimb_description, reimb_receipt, s.reimb_status, t.reimb_type
-    FROM ers_reimbursement reimb
-    JOIN ers_reimbursement_type t
-        ON reimb.reimb_type_id = t.reimb_type_id
-    JOIN ers_reimbursement_status s
-        ON reimb.reimb_status_id = s.reimb_status_id;
-
-SELECT reimb_id, reimb_amount, reimb_submitted, reimb_description, reimb_receipt, 
-    u.user_first_name as author_fn, u.user_last_name as author_ln
-FROM ers_reimbursement r
-JOIN ers_users u
-ON r.reimb_author = u.ers_user_id;
 
 --Reimbursement with status, type, and user author
 SELECT reimb_id, reimb_amount, reimb_submitted, reimb_description, reimb_receipt, s.reimb_status, t.reimb_type,
@@ -56,7 +40,8 @@ SELECT reimb_id, reimb_amount, reimb_submitted, reimb_description, reimb_receipt
         ON reimb.reimb_status_id = s.reimb_status_id
     JOIN ers_users u
     ON reimb.reimb_author = u.ers_user_id;
-    
+
+--Reimbursement with status, type, and user author and resolver
 SELECT reimb_id, reimb_amount, reimb_submitted, reimb_resolved , reimb_description, reimb_receipt, s.reimb_status, t.reimb_type,
         u.user_first_name as author_fn, u.user_last_name as author_ln, 
         ad.user_first_name as resolver_fn, ad.user_last_name as resolver_ln
@@ -112,14 +97,3 @@ END;
 /
     
 ----
-SELECT reimb_id, reimb_amount, reimb_submitted, reimb_description, reimb_receipt, s.reimb_status, t.reimb_type,
-    u.user_first_name as author_fn, u.user_last_name as author_ln, ad.user_first_name, ad.user_last_name
-    FROM ers_reimbursement reimb
-    JOIN ers_reimbursement_type t
-        ON reimb.reimb_type_id = t.reimb_type_id
-    JOIN ers_reimbursement_status s
-        ON reimb.reimb_status_id = s.reimb_status_id
-    JOIN ers_users u
-    ON reimb.reimb_author = u.ers_user_id
-    JOIN ers_users ad
-    ON reimb.reimb_author = ad.ers_user_id;
