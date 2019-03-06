1808e012336ac4bc0d0ea5f2b5083b9a0a5ebefe
Zachary Marazita <marazitazachary@gmail.com>
2019-03-05 22:12:07 -0500

    done with first round of DAOs

--- Project 1 STS Workspace/Zachary-Jaitee-Project1/src/main/java/com/revature/drivers/Project1Driver.java
+++ Project 1 STS Workspace/Zachary-Jaitee-Project1/src/main/java/com/revature/drivers/Project1Driver.java
@@ -11,8 +11,30 @@ public class Project1Driver {
 		System.out.println("Hello Project 1!");
 		
 		TicketDAO ticketDao = new TicketDAO();
+		ArrayList<Ticket> tlist = new ArrayList<>();
 		
-		System.out.println(ticketDao.getById(42));
-		System.out.println(ticketDao.getById(4));
+		System.out.println("Original list:");
+		
+		Ticket t1 = ticketDao.getById(23);
+		System.out.println(t1);
+		t1.setStatus(ReimbursementStatus.DENIED);
+		t1.setResolverId(25);
+		
+//		Ticket t2 = ticketDao.getById(42);
+//		System.out.println(t2);
+//		t2.setStatus(ReimbursementStatus.DENIED);
+//		t2.setResolverId(25);
+		
+		tlist.add(t1);
+//		tlist.add(t2);
+		
+		
+		System.out.println("\n");
+		System.out.println("New List:");
+		ArrayList<Ticket> updatedList = ticketDao.update(tlist);
+		
+		for(Ticket t : updatedList) {
+			System.out.println(t);
+		}
 	}
 }
