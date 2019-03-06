f675a02b3c72402cb284ff2465c5183349a167e4
Jaitee Pitts <jaiteepitts@gmail.com>
2019-03-06 09:41:29 -0500

    created service and servlet classes for user

---
+++ Project 1 STS Workspace/Zachary-Jaitee-Project1/src/main/java/com/revature/servlets/UserServlet.java
@@ -0,0 +1,16 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UserServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException{
+		
+	}
+
+}
