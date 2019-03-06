efc847516c51a38271e7bed7b2c94f59b8be9493
Jaitee Pitts <jaiteepitts@gmail.com>
2019-03-06 10:12:55 -0500

    created connection to the server via userServlet

---
+++ Project 1 STS Workspace/Zachary-Jaitee-Project1/src/main/java/com/revature/servlets/ViewServlet.java
@@ -0,0 +1,18 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ViewServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException{
+		
+		
+		
+	}
+
+}
