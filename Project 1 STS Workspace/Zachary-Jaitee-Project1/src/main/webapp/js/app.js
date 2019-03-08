c49fb7b6541f18e8983be55c53a5701f4b3ce5bf
Zachary Marazita <marazitazachary@gmail.com>
2019-03-08 12:16:12 -0500

    almost working login

--- Project 1 STS Workspace/Zachary-Jaitee-Project1/src/main/webapp/js/app.js
+++ Project 1 STS Workspace/Zachary-Jaitee-Project1/src/main/webapp/js/app.js
@@ -6,26 +6,6 @@ window.onload = () =>{
     loadLogin();
 }
 
-//get request to view servlet
-async function loadLogin(){
-    console.log('loading login...')
-
-    appbody.innerHTML = await fetchView('login.view');
-    configureLogin();
-}
-
-async function login (){
-    //need to get jwt from response somehow???
-    loadDashboard();
-
-}
-
-function configureLogin(){
-    console.log('configuring login...');
-    document.getElementById('sign-in-button').addEventListener('click', login);
-    document.getElementById('register').addEventListener('click', loadRegister);
-}
-
 //helper method that gets the view for all the methods
 async function fetchView(uri) {
     console.log("in fetchView()");
@@ -39,6 +19,63 @@ async function fetchView(uri) {
     return await response.text();
 }
 
+// LOGIN
+//----------------------------------------------------------------------------------------------------------
+//get request to view servlet
+async function loadLogin(){
+    console.log('loading login...')
+
+    appbody.innerHTML = await fetchView('login.view');
+    configureLogin();
+}
+
+function configureLogin(){
+    console.log('configuring login...');
+    document.getElementById('login-button').addEventListener('click', login);
+    document.getElementById('register').addEventListener('click', loadRegister);
+}
+
+async function login (){
+    //need to get jwt from response somehow???
+    // loadDashboard();
+    console.log('inLogin');
+    // let xhr = new XMLHttpRequest;
+    // xhr.open('POST', 'auth', true);
+    // xhr.send({
+    //     username: document.getElementById('username'),
+    //     password: document.getElementById('password')
+    // });
+    // console.log('sent');
+    // xhr.onreadystatechange = () => {
+    //     console.log('readystate: '+ xhr.readyState +' statuscode: '+ xhr.status);
+    //     if (xhr.readyState == 4 && xhr.status == 200) {
+    //         let userDetails = JSON.parse(xhr.responseText);
+    //         console.log(userDetails);
+    //         // checkLogin(userDetails);
+    //     }
+    // }
+    let credentials = [];
+   credentials.push(document.getElementById('username').value);
+   credentials.push(document.getElementById('password').value);
+   console.log(credentials[0] +" "+credentials[1]);
+    let response = await fetch('auth', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    });
+    console.log(JSON.stringify(credentials));
+    console.log(response)
+}
+
+
+//------------------------------------------------------------------------------------------------------------------
+
+
+// REGISTER
+//-----------------------------------------------------------------------------------------------------------------
 async function loadRegister(){
     appbody.innerHTML = await fetchView('register.view');
     configureRegister();
@@ -54,7 +91,12 @@ async function loadDashboard(){
     appbody.innerHTML = await fetchView('dashboard.view');
     configureRegister();
 }
+//------------------------------------------------------------------------------------------------------------------
 
+
+// DASHBOARD
+//-------------------------------------------------------------------------------------------------------------------
 function configureDashboard(){
     SOURCE.src = 'dashboard.js';
 }
+//-------------------------------------------------------------------------------------------------------------------
