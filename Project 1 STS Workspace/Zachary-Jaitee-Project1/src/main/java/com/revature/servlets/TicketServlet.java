3d583d0f9e09804688869dd6096e0f28aab78568
Zachary Marazita <marazitazachary@gmail.com>
2019-03-11 20:53:07 -0400

    added ticket adding funcitonality to TicketServlet doPost()

--- Project 1 STS Workspace/Zachary-Jaitee-Project1/src/main/java/com/revature/servlets/TicketServlet.java
+++ Project 1 STS Workspace/Zachary-Jaitee-Project1/src/main/java/com/revature/servlets/TicketServlet.java
@@ -16,6 +16,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.models.Principal;
 import com.revature.models.Ticket;
+import com.revature.models.enums.ReimbursementType;
 import com.revature.service.TicketService;
 
 @WebServlet("/ticket")
@@ -91,12 +92,22 @@ public class TicketServlet extends HttpServlet{
 			
 			ticket.setAuthorId(Integer.parseInt(ticketData[0]));
 			ticket.setAmount(Double.parseDouble(ticketData[1]));
+			ticket.setType(ReimbursementType.valueOf(ticketData[2]));
+			ticket.setTicketDescription(ticketData[3]);
 			
-			
+			ticket = service.add(ticket);
+			if(ticket != null) {
+				resp.setStatus(200);
+			}else {
+				resp.setStatus(400);
+			}
 			
 			
 		}catch(Exception e) {
 			e.printStackTrace();
+			log.error(e.getMessage());
+			resp.setStatus(500);
+			return;
 		}
 	}
 }
